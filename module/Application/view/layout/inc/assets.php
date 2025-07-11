116e6c6990e6f715dac9eb568bf69f178c0d8094
elijahcjives <elijah@bayounorth.com>
2025-07-11 18:54:04 -0400

    Login style

--- module/Application/view/layout/inc/assets.php
+++ module/Application/view/layout/inc/assets.php
@@ -1,22 +1,23 @@
 <?php ?>
 
-<!--jQuery-->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js">
-<link href=="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.14.1/jquery-ui.min.js">
-
 <!--Bootstrap-->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/js/bootstrap.min.js">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.13.1/font/bootstrap-icons.min.css">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
 
 <!--UNPKG-->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.8/umd/popper.min.js">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/tippy.js/6.3.7/tippy.umd.min.js">
+<script src="https://unpkg.com/tippy.js@6"></script>
 
 <!--Cloudflare-->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js">
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js" defer></script>
 
 <!--Custom-->
-<link href="/css/style.css" >
-<link href="/js/main.js" >
+<script src="/js/main.js" ></script>
 
+<!--jQuery-->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"  crossorigin="anonymous"></script>
+<script  src="https://code.jquery.com/ui/1.13.1/jquery-ui.min.js"  crossorigin="anonymous"></script>
+
+<!--Bootstrap-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css" />
